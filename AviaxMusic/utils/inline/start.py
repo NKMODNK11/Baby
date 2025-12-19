6764e499aacd2a9f5760293d64792599cf81f91b
NKMODNK11 <j19502570@gmail.com>
2025-12-20 03:18:13 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -25,11 +25,7 @@ def private_panel(_):
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            #InlineKeyboardButton(
-                text="𝖠𝖯𝖨 𝖢𝗈𝗇𝗌𝗈𝗅𝖾",
-                callback_data="api_console"
-           # )
+        
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
@@ -41,6 +37,7 @@ def private_panel(_):
         ],
     ]
     return buttons
+
 
 
 
