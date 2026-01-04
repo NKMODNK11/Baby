b507fae5f32b59253450f0270f783b9bc035a1a6
anony <notanony@hotmail.com>
2026-01-04 13:48:26 +0530

    Refactor call.py to improve structure and clarity

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -107,8 +107,8 @@ class Call(PyTgCalls):
         try:
             await _clear_(chat_id)
             await assistant.leave_call(chat_id)
-        except Exception as ex:
-            raise Exception(f"Error: {ex}")
+        except Exception:
+            pass
 
     async def stop_stream_force(self, chat_id: int):
         for client in self.clients:
@@ -551,6 +551,3 @@ class Call(PyTgCalls):
 
 
 Aviax = Call()
-
-
-
